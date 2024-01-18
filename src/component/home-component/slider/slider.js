a39932421a58a902ed4d0aa7875411c99fca8433
shaif-ali-javad <shaifalijavad60@gmail.com>
2024-01-18 20:44:34 +0530

    jds

--- src/component/home-component/slider/slider.js
+++ src/component/home-component/slider/slider.js
@@ -8,16 +8,15 @@ import "./slider.css";
 
 import "swiper/css";
 
-import slider0 from "../../img/slider0.jpg";
-import slider1 from "../../img/slider1.jpeg";
-import slider2 from "../../img/slider2.jpeg";
+// import slider0 from "../../img/slider0.jpg";
+// import slider1 from "../../img/slider1.jpeg";
+// import slider2 from "../../img/slider2.jpeg";
 
 const Slider = () => {
   return (
     <>
       <div style={{ width: "100%", color: "black" }} className="slider-contain">
         <Swiper
-          
           navigation={true}
           autoplay={{
             delay: 9000,
@@ -27,13 +26,13 @@ const Slider = () => {
           modules={[Autoplay]}
         >
           <SwiperSlide>
-            <img src={slider0} style={{ width: "100%" }} alt="" />
+            <div className="img--0"></div>
           </SwiperSlide>
           <SwiperSlide>
-            <img src={slider1} style={{ width: "100%" }} alt="" />
+            <div className="img--1"></div>
           </SwiperSlide>
           <SwiperSlide>
-            <img src={slider2} style={{ width: "100%" }} alt="" />
+            <div className="img--2"></div>
           </SwiperSlide>
         </Swiper>
       </div>
